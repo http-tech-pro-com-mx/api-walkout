6d63bb0f5ec85354ebe71d027dcdd2f01c599181
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2020-02-07 18:40:25 -0600

    Cierre viernes [07-feb-2020]

--- src/main/java/com/tech/pro/walker/api/models/entity/IP.java
+++ src/main/java/com/tech/pro/walker/api/models/entity/IP.java
@@ -45,8 +45,32 @@ public class IP  implements Serializable {
 	private int QC;
 	
 	
-	@Column
+	@Column(nullable = false)
+	private Date fecha_asignacion;
+	
+	@Column(nullable = true)
+	private Date fecha_envio_campo;
+	
+	@Column(nullable = true)
 	private Date fecha_levantamiento;
+	
+	@Column(nullable = true)
+	private Date fecha_qc;
+	
+	@Column(nullable = true)
+	private Date fecha_cliente;
+	
+	@Column(nullable = true)
+	private Date fecha_shared_point;
+	
+	@Column(nullable = true)
+	private int total_grids;
+	
+	@Column(columnDefinition = "boolean default false", nullable = false)
+	private boolean actualizacion;
+	
+	@Column
+	private Double km_actualizados;
 	
 	@Column
 	private int tipo;
@@ -69,6 +93,7 @@ public class IP  implements Serializable {
 	
 	@Column
 	private Long usuario_update;
+	
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name =  "id_proyecto")
@@ -95,75 +120,158 @@ public class IP  implements Serializable {
 	public Double getPies() {
 		return pies;
 	}
+	
 	public void setPies(Double pies) {
 		this.pies = pies;
 	}
 	public int getQC() {
 		return QC;
 	}
+	
 	public void setQC(int qC) {
 		QC = qC;
 	}
+	
 	public String getUbicacion() {
 		return ubicacion;
 	}
+	
 	public void setUbicacion(String ubicacion) {
 		this.ubicacion = ubicacion;
 	}
+	
 	public boolean isEstatus() {
 		return estatus;
 	}
+	
 	public void setEstatus(boolean estatus) {
 		this.estatus = estatus;
 	}
+	
 	public Date getFecha_registro() {
 		return fecha_registro;
 	}
+	
 	public void setFecha_registro(Date fecha_registro) {
 		this.fecha_registro = fecha_registro;
 	}
+	
 	public Long getUsuario_registro() {
 		return usuario_registro;
 	}
+	
 	public void setUsuario_registro(Long usuario_registro) {
 		this.usuario_registro = usuario_registro;
 	}
+	
 	public Date getFecha_update() {
 		return fecha_update;
 	}
+	
 	public void setFecha_update(Date fecha_update) {
 		this.fecha_update = fecha_update;
 	}
+	
 	public Long getUsuario_update() {
 		return usuario_update;
 	}
+	
 	public void setUsuario_update(Long usuario_update) {
 		this.usuario_update = usuario_update;
 	}
+	
 	public Proyecto getProyecto() {
 		return proyecto;
 	}
 	public void setProyecto(Proyecto proyecto) {
 		this.proyecto = proyecto;
 	}
+	
 	public List<Grid> getGrids() {
 		return grids;
 	}
+	
 	public void setGrids(List<Grid> grids) {
 		this.grids = grids;
 	}
+	
 	public Date getFecha_levantamiento() {
 		return fecha_levantamiento;
 	}
 	public void setFecha_levantamiento(Date fecha_levantamiento) {
 		this.fecha_levantamiento = fecha_levantamiento;
 	}
+	
 	public int getTipo() {
 		return tipo;
 	}
+	
 	public void setTipo(int tipo) {
 		this.tipo = tipo;
 	}
+	
+	public Date getFecha_asignacion() {
+		return fecha_asignacion;
+	}
+	
+	public void setFecha_asignacion(Date fecha_asignacion) {
+		this.fecha_asignacion = fecha_asignacion;
+	}
+	
+	public Date getFecha_envio_campo() {
+		return fecha_envio_campo;
+	}
+	
+	public void setFecha_envio_campo(Date fecha_envio_campo) {
+		this.fecha_envio_campo = fecha_envio_campo;
+	}
+	
+	public Date getFecha_qc() {
+		return fecha_qc;
+	}
+	
+	public void setFecha_qc(Date fecha_qc) {
+		this.fecha_qc = fecha_qc;
+	}
+	
+	public Date getFecha_cliente() {
+		return fecha_cliente;
+	}
+	
+	public void setFecha_cliente(Date fecha_cliente) {
+		this.fecha_cliente = fecha_cliente;
+	}
+	
+	
+	public Date getFecha_shared_point() {
+		return fecha_shared_point;
+	}
+	public void setFecha_shared_point(Date fecha_shared_point) {
+		this.fecha_shared_point = fecha_shared_point;
+	}
+	public int getTotal_grids() {
+		return total_grids;
+	}
+	public void setTotal_grids(int total_grids) {
+		this.total_grids = total_grids;
+	}
+	public boolean isActualizacion() {
+		return actualizacion;
+	}
+	
+	public void setActualizacion(boolean actualizacion) {
+		this.actualizacion = actualizacion;
+	}
+	
+	public Double getKm_actualizados() {
+		return km_actualizados;
+	}
+	
+	public void setKm_actualizados(Double km_actualizados) {
+		this.km_actualizados = km_actualizados;
+	}
+	
+	
 	
 	
 	
